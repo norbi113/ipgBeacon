fa20e45c99172511d25fb343f7baf8174e208fd9
norbi113 <norbert.maz@gmail.com>
2018-02-14 14:29:50 +0100

    added proximity-sdk

--- app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
+++ app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.mbww.ipgbeacon
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
