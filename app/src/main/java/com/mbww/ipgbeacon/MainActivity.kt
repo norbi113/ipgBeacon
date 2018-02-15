7ed1c1384660d2b9a2c077bf6525bea0faed5731
norbi113 <norbert.maz@gmail.com>
2018-02-15 11:24:35 +0100

    added telemetry scanner, permissions in manifest but with error: java.lang.SecurityException: Need ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION permission to get scan results

--- app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
+++ app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
@@ -2,6 +2,9 @@ package com.mbww.ipgbeacon
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.estimote.cloud_plugin.common.EstimoteCloudCredentials
+import com.estimote.scanning_sdk.api.EstimoteBluetoothScannerFactory
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cloudCredentials = EstimoteCloudCredentials("ipgbeacon-6iu" , "09fa4b5a21e4267d566b1b5485ad50c4")
+        val scanner = EstimoteBluetoothScannerFactory(applicationContext).getSimpleScanner()
+        val scanHandler = scanner.estimoteTelemetryFullScan()
+                .withBalancedPowerMode()
+                .withOnPacketFoundAction {
+                    Log.d("Full Telemetry", "Got Full Telemetry packet: $it")
+                }
+                .start()
     }
 }
