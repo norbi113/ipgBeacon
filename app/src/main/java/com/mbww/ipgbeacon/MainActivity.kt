a84bf6262d861adf1638ea156c677eaa7c4013d6
norbi113 <norbert.maz@gmail.com>
2018-02-19 13:38:41 +0100

    added runtime permission

--- app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
+++ app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
@@ -3,9 +3,11 @@ package com.mbww.ipgbeacon
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.TextView
 import com.estimote.cloud_plugin.common.EstimoteCloudCredentials
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory
+import com.estimote.proximity_sdk.proximity.ProximityObserverBuilder
 import com.estimote.scanning_sdk.api.EstimoteBluetoothScannerFactory
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val cloudCredentials = EstimoteCloudCredentials("ipgbeacon-6iu" , "09fa4b5a21e4267d566b1b5485ad50c4")
+        val proximityObserver = ProximityObserverBuilder(applicationContext, cloudCredentials)
+                .withOnErrorAction { throwable ->
+                    Log.e("app", "proximity observer error: " + throwable)
+                    null
+                }
+                .withBalancedPowerMode()
+                .build()
         val scanner = EstimoteBluetoothScannerFactory(applicationContext).getSimpleScanner()
         val scanHandler = scanner.estimoteTelemetryFullScan()
                 .withBalancedPowerMode()
@@ -20,5 +29,29 @@ class MainActivity : AppCompatActivity() {
                     Log.d("Full Telemetry", "Got Full Telemetry packet: $it")
                 }
                 .start()
+
+        RequirementsWizardFactory
+                .createEstimoteRequirementsWizard()
+                .fulfillRequirements(applicationContext,
+                        // onRequirementsFulfilled
+                        {
+                            Log.d("app", "requirements fulfilled")
+                            proximityObserver.start()
+                            null
+                        },
+                        // onRequirementsMissing
+                        { requirements ->
+                            Log.e("app", "requirements missing: " + requirements)
+                            null
+                        }
+                        // onError
+                ) { throwable ->
+                    Log.e("app", "requirements error: " + throwable)
+                    null
+                }
+
+
     }
+
+
 }
