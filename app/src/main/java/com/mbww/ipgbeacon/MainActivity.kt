ec1ddc22be847634766adef098aa3cf7117885db
norbi113 <norbert.maz@gmail.com>
2018-02-20 14:45:58 +0100

    added firebase analytics

--- app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
+++ app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.estimote.scanning_sdk.api.EstimoteBluetoothScannerFactory
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
             val scanner = EstimoteBluetoothScannerFactory(applicationContext).getSimpleScanner()
             val scanHandler = scanner.estimoteTelemetryFullScan()
                     .withBalancedPowerMode()
