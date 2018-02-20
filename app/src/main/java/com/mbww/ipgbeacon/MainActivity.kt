0ef73477e3cb6296957765406215ac3316921a7c
norbi113 <norbert.maz@gmail.com>
2018-02-20 17:13:45 +0100

    trying to add temperature as double to textview

--- app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
+++ app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
@@ -5,33 +5,20 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.TextView
 import com.estimote.scanning_sdk.api.EstimoteBluetoothScannerFactory
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
-    private var mFirebaseAnalytics: FirebaseAnalytics? = null
-
-    override fun onCreate(savedInstanceState: Bundle?): Double {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
         val scanner = EstimoteBluetoothScannerFactory(applicationContext).getSimpleScanner()
             val scanHandler = scanner.estimoteTelemetryFullScan()
                     .withBalancedPowerMode()
                     .withOnPacketFoundAction {
                         Log.d("Full Telemetry", "Got Full Telemetry packet: $it")
-                        var temperature = it.temperatureInCelsiusDegrees
-                        val textView: TextView = findViewById<TextView>(R.id.telemetryData)
-                        textView.setOnClickListener {
-                            textView.text = temperature
-                        }
+                        val temperature = it.temperatureInCelsiusDegrees
+                        var textView: TextView = findViewById<TextView>(R.id.telemetryData)
                     }
                     .start()
-
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("message")
-
-        myRef.setValue("Hello, World!")
     }
 }
