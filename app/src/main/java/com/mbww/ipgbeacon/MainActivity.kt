ff7afcb39736ea0bad402db029bf75f2ed910e5e
norbi113 <norbert.maz@gmail.com>
2018-02-22 17:02:02 +0100

    Working telemetry readout to textview

--- app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
+++ app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
@@ -2,7 +2,6 @@ package com.mbww.ipgbeacon
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.TextView
 import com.estimote.scanning_sdk.api.EstimoteBluetoothScannerFactory
 
@@ -15,9 +14,16 @@ class MainActivity : AppCompatActivity() {
             val scanHandler = scanner.estimoteTelemetryFullScan()
                     .withBalancedPowerMode()
                     .withOnPacketFoundAction {
-                        Log.d("Full Telemetry", "Got Full Telemetry packet: $it")
-                        val temperature = it.temperatureInCelsiusDegrees
-                        var textView: TextView = findViewById<TextView>(R.id.telemetryData)
+                        var temperature: TextView = findViewById<TextView>(R.id.temperature)
+                        temperature.setText("Temperature: ${it.temperatureInCelsiusDegrees}")
+                        var magnetometer: TextView = findViewById<TextView>(R.id.magnetometer)
+                        magnetometer.setText("Magnetometer: ${it.magnetometer}")
+                        var pressure: TextView = findViewById<TextView>(R.id.pressure)
+                        pressure.setText("Pressure: ${it.pressure}")
+                        var ambientLight: TextView = findViewById<TextView>(R.id.ambientLight)
+                        ambientLight.setText("Ambient Light: ${it.ambientLightInLux}")
+                        var motionState: TextView = findViewById<TextView>(R.id.motionState)
+                        motionState.setText("Motion State: ${it.motionState}")
                     }
                     .start()
     }
