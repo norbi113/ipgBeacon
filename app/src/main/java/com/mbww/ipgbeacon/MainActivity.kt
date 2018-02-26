4c5f7f176144e0186df1465db773eb050f7f0a62
norbi113 <norbert.maz@gmail.com>
2018-02-26 15:24:21 +0100

    Added if statements to check telemetry thresholds

--- app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
+++ app/src/main/java/com/mbww/ipgbeacon/MainActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 import com.estimote.scanning_sdk.api.EstimoteBluetoothScannerFactory
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,12 +28,42 @@ class MainActivity : AppCompatActivity() {
                         ambientLight.setText("Ambient Light: ${it.ambientLightInLux}")
                         var motionState: TextView = findViewById<TextView>(R.id.motionState)
                         motionState.setText("Motion State: ${it.motionState}")
+                        if(it.temperatureInCelsiusDegrees > 6) {
+                            var checkTemperature: TextView = findViewById<TextView>(R.id.checkTemperature)
+                            checkTemperature.setText("Temperature high!")
+                        }else{
+                            var checkTemperature: TextView = findViewById<TextView>(R.id.checkTemperature)
+                            checkTemperature.setText("Temperature low!")
+                        }
+                        if(it.magnetometer.xAxis >= 0 && it.magnetometer.yAxis >= 0 && it.magnetometer.zAxis >= 0) {
+                            var checkMagnetometer: TextView = findViewById<TextView>(R.id.checkMagnetometer)
+                            checkMagnetometer.setText("Magnetometer is crazy!")
+                        }else{
+                            var checkMagnetometer: TextView = findViewById<TextView>(R.id.checkMagnetometer)
+                            checkMagnetometer.setText("Magnetometer is calm!")
+                        }
+                        if(it.pressure > 6) {
+                            var checkPressure: TextView = findViewById<TextView>(R.id.checkPressure)
+                            checkPressure.setText("Pressure higher!")
+                        }else{
+                            var checkPresure: TextView = findViewById<TextView>(R.id.checkPressure)
+                            checkPresure.setText("Pressure lower!")
+                        }
+                        if(it.ambientLightInLux > 6) {
+                            var checkLight: TextView = findViewById<TextView>(R.id.checkLight)
+                            checkLight.setText("Lights on!")
+                        }else{
+                            var checkLight: TextView = findViewById<TextView>(R.id.checkLight)
+                            checkLight.setText("Lights off!")
+                        }
+                        if(it.motionState == true) {
+                            var checkMotion: TextView = findViewById<TextView>(R.id.checkMotion)
+                            checkMotion.setText("Motion on!")
+                        }else{
+                            var checkMotion: TextView = findViewById<TextView>(R.id.checkMotion)
+                            checkMotion.setText("Motion off!")
+                        }
                     }
                     .start()
-
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("message")
-
-        myRef.setValue("Hello, World!")
     }
 }
